39b4b85115cacce44529ffccd58dbb6610e21fd8
fuadagus <68749493+fuadagus@users.noreply.github.com>
2021-08-06 00:28:13 +0700

    Update app.js

--- app.js
+++ app.js
@@ -6,4 +6,6 @@ atOptions = {
 		'width' : 160,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivedisplayformat.com/83f698221403855441e068ad4e9a5f75/invoke.js"></scr' + 'ipt>');
+function alertMessage('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivedisplayformat.com/83f698221403855441e068ad4e9a5f75/invoke.js"></scr' + 'ipt>') {
+    alert('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivedisplayformat.com/83f698221403855441e068ad4e9a5f75/invoke.js"></scr' + 'ipt>')
+}
